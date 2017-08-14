782554d0f7e0916081e3064636621eb9cc5c2fea
heli <294813868@qq.com>
2017-08-14 09:29:05 +0800

    推送配置修改
    配置appKey 和appMasterSecret

--- src/main/java/zn/until/upush/UPushConfig.java
+++ src/main/java/zn/until/upush/UPushConfig.java
@@ -1,8 +1,8 @@
 package zn.until.upush;
 
 public class UPushConfig {
-  public static final String appkey_android = "afe596d144e99491a66f2169";
-  public static final String appMasterSecret_android = "45742427c5a0660930e4993a";
+  public static final String appkey_android = "5990fa4cc62dca5d6500191d";
+  public static final String appMasterSecret_android = "lmic4lku6fg7per9uqkwhrknhihlkkqi";
    
   public static final String appkey_ios="";
   public static final String appMasterSecret_ios="";
